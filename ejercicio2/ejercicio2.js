3af926b5f479d55952035b80c38c1bd3beec6347
davidlopezcorsino <dalocorsino@gmail.com>
2023-01-26 23:29:58 +0100

    Ejer2y3DLC

---
+++ ejercicio2/ejercicio2.js
@@ -0,0 +1,6 @@
+const cumple = {
+    day: 24,
+    month: 'Noviembre'
+}
+
+console.log("Mi cumple es el " +  cumple.day + " de " + cumple["month"]);
---
+++ ejercicio3/ejercicio3.js
@@ -0,0 +1,13 @@
+const data = ["hola", 2, 5, "adios"];
+console.log("numero mayor",data[2]);
+
+const suma = data[1] + data[2];
+console.log("suma",suma);
+const resta = data[2] - data[1];
+console.log("resta",resta);
+const producto = data[2] * data[1];
+console.log("producto",producto);
+const cociente = data[2] / data[1];
+console.log("cociente",cociente);
+const resto = data[2] % data[1];
+console.log("resto",resto);
