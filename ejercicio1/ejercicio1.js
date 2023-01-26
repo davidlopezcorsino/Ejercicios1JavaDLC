e7e2b0eb7eeb3cbf9a46af64a43d1df8ea0f7d9b
davidlopezcorsino <dalocorsino@gmail.com>
2023-01-26 20:36:14 +0100

    Ejercicio1DLCparte2

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -11,3 +11,15 @@ const areaLateral= 23 * 13;
 const areaBase= 16 * 23;
 const areaTotal= areaLateral + 2*areaBase;
 console.log('resultadoArea',areaTotal + ' cm2');
+
+
+//Generalización
+const base =23;
+const altura =13;
+const ancho=16;
+
+const volumen2 = base * altura * ancho;
+console.log('ResultadoVolumen2',volumen2);
+
+const area2 = base * altura + 2 * (ancho * base);
+console.log('ResultadoArea2',area2);
